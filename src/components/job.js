51a1762a3a91f92e3da8a778541cb3027c47b6d0
Santiago <s.morenom@uniandes.edu.co>
2021-11-29 18:02:24 -0500

    owari

--- src/components/job.js
+++ src/components/job.js
@@ -1,14 +1,39 @@
 import React from "react";
-
+import { FormattedDate } from "react-intl";
+import { FormattedMessage } from "react-intl";
 const Job = (props) => {
+  var millon = "million";
+ 
+  if(navigator.language.includes('es')){
+    if (props.offer.salary === 1) {
+      millon = "millón";
+    } else if(props.offer.salary>1) {
+      millon = "millones";
+    }
+    
+  }
+console.log(props.offer.views.toLocaleString('en', { maximumSignificantDigits: 3 }));
   return (
     <tr>
       <th scope="row">{props.offer.id}</th>
       <td>{props.offer.name}</td>
       <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
+      <td>{props.offer.salary +" "}<FormattedMessage id={millon}/>
+      </td>
       <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td>
+  <FormattedDate
+    value={new Date(props.offer.date)}
+    year='numeric'
+    month='long'
+    day='numeric'
+    weekday='long'
+  />
+  </td>
+   <td>
+        <FormattedMessage id={props.offer.views.toLocaleString('en',{ maximumSignificantDigits: 3 }, {maximumFractionDigits:0})} />
+      </td>
+
     </tr>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import JobsList from "./components/jobslist";
-
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+import {IntlProvider} from 'react-intl';
+import JobsList from './components/jobslist'
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
+var locale;
+var mensajes;
+if(navigator.language.includes('es')){
+    locale="es-ES";
+    mensajes=localeEsMessages;
+}else{
+    locale="en-US";
+    mensajes=localeEnMessages;
+}
+ReactDOM.render(
+	<IntlProvider locale={locale} messages= {mensajes}>
+		<JobsList/>
+	</IntlProvider>, document.getElementById("root")
+);
